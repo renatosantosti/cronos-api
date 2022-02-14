51b1634146b5325322a1a1a9405d50aca5faefd1
Renato Santos <renatosantos.ti@gmail.com>
2022-02-14 01:22:02 -0300

    fix - ajustando validação mock de validação de token de autorização na rota

--- src/middleware/autenticacao.js
+++ src/middleware/autenticacao.js
@@ -5,25 +5,24 @@
  */
 
 const validarAutorizacaoAcesso = (req, res, next) => {
-  const token = req.headers.authorization;
+  // const token = req.headers.authorization;
+  const token = '....'; // @TODO obter token da requisição
   if (!token)
     res
       .status(401)
       .send({ erro: 'Token não informado, obtenha a autorização de acesso!' });
   else {
     // @TODO buscar usuário com base no token
-    const user = {};
+    const user = { nome: 'João', isAdmin: true };
     if (!user)
       res.status(401).send({
         erro: 'Token expirado ou inválido, obtenha a autorização de acesso!'
       });
     else {
       req.user = user;
-      next();
+      return next();
     }
   }
-
-  if (token) return true;
 };
 
 export default validarAutorizacaoAcesso;
